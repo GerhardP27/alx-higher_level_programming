2ff9265016397b1275a1641ce52806a222f72feb
Gerhard Pillay <gerhardpillay.gp@gmail.com>
2022-12-06 12:19:25 +0200

    Fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -16,4 +16,3 @@ if __name__ == '__main__':
         print(rows)
     cr.close()
     db.close()
-    
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -21,4 +21,3 @@ if __name__ == '__main__':
         print(rows)
     cr.close()
     db.close()
-    
