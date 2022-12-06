ae3aa9d28b99cb83c2c20f2fb320d1093dcfa790
Gerhard Pillay <gerhardpillay.gp@gmail.com>
2022-12-06 12:57:43 +0200

    Test

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""Script that prints all City objects from the database hbtn_0e_14_usa"""
-import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
-
+"""Script to rpint all City objects from database `hbtn_0e_14_usa`."""
 
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(host="localhost", port=3306, sys.argv[1], sys.argv[2],
-                                sys.argv[3], pool_pre_ping=True))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_city import City
+    from model_state import State
+
+    engine = create_engine('mysql+mysqldb://'
+                           '{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]))
+    Session = sessionmaker(bind=engine)
     session = Session()
-    res = session.query(State, City).filter(State.id == City.state_id) \
-                 .order_by(City.id).all()
-    for states, city in res:
-        print(f"{states.name}: ({city.id}) {city.name}")
-    session.close()
+    for city, state_name in session.query(City, State.name)\
+                                   .join(State, State.id == City.state_id):
+        print("{}: ({}) {}".format(state_name, city.id, city.name))
